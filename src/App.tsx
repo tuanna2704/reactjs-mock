dfd3e2606f5b11129bf19349d8681be95e3f18f6
anhth34 <tranhunganh197@gmail.com>
2022-05-29 21:09:58 +0700

    config redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Counter } from './component/counter/counter';
 
 function App() {
+  let title : string = 'MOCK PROJECT REACTJS'
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='d-flex p-3 justify-content-evenly'> 
+        <h1>{title}</h1>
+        <Counter></Counter>
+      </div>
     </div>
   );
 }
