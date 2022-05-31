f52dcaa96895a9923526d4d3cc6b439c16a7b4f3
anhth34 <tranhunganh197@gmail.com>
2022-05-31 14:50:45 +0700

    update add cpn

--- src/component/list-repo/add-repo/add-repo.tsx
+++ src/component/list-repo/add-repo/add-repo.tsx
@@ -1,8 +1,18 @@
-function AddRepoComponent() {
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import './add-repo.css'
 
+function AddRepoComponent() {
+  const navigate = useNavigate();
   return (
     <div>
-        ADD COMPONENT
+        <Button className='button-back mt-3' onClick={() => navigate('/list-repo')}><ChevronRightIcon></ChevronRightIcon> Back</Button>
+        <div className='d-flex justify-content-center align-items-center'>
+          <div>
+            <h2>ADD ITEM</h2>
+          </div>
+        </div>
     </div>
   );
 }
